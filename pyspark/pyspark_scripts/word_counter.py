504261e6fc878968322ff9439d40604cb58e7995
joaofsoares <joaofsoares@outlook.com>
2018-11-17 22:11:39 +0100

    PySpark - passing parameter as file to spark batch

--- pyspark/pyspark_scripts/word_counter.py
+++ pyspark/pyspark_scripts/word_counter.py
@@ -8,28 +8,43 @@ Execute: spark-submit.cmd --master="local[*]" script_directory/word_counter.py
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import desc
 import re
+import sys
 
 
-def is_non_empty(s: str) -> bool:
-    return True if s else False
+def execute_spark(file_name):
+    """
+    Execute spark batch
+    """
+    def is_non_empty(s: str) -> bool:
+        """
+        Check if the string is not empty
+        """
+        return True if s else False
 
 
-def clean_str(s: str) -> str:
-    return "".join(filter(lambda c: c.isalnum(), s))
+    def clean_str(s: str) -> str:
+        """
+        Clean special characters from the string
+        """
+        return "".join(filter(lambda c: c.isalnum(), s))
 
 
-spark = SparkSession.builder.appName("Word Counter").getOrCreate()
+    spark = SparkSession.builder.appName("Word Counter").getOrCreate()
 
-df = spark.read.option("inferSchema", "true").format("text")\
-    .load("file_path")
+    df = spark.read.option("inferSchema", "true").format("text")\
+        .load(file_name)
 
-m_df = df.rdd.flatMap(lambda row : re.split("\W+", row[0]))\
-    .map(lambda w: clean_str(w))\
-    .filter(lambda w: is_non_empty(w))\
-    .map(lambda w: (w, 1))\
-    .toDF(["word", "count"]).cache()
+    m_df = df.rdd.flatMap(lambda row : re.split("\W+", row[0]))\
+        .map(lambda w: clean_str(w))\
+        .filter(lambda w: is_non_empty(w))\
+        .map(lambda w: (w, 1))\
+        .toDF(["word", "count"]).cache()
 
 
-m_df.groupBy("word").count().orderBy(desc("count")).show()
+    m_df.groupBy("word").count().orderBy(desc("count")).show()
 
-spark.stop()
+    spark.stop()
+
+
+if __name__ == "__main__":
+    execute_spark(sys.argv[1])
